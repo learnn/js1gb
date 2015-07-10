b005bcaa3a89b81f9a4c6fc1440d2032997939ac
Bharat Shetty Barkur <bharat.shetty@gmail.com>
2015-07-10 23:30:41 +0530

    Ensure unique .js files in training data corpus

---
+++ extractFilesWithExtensions.py
@@ -0,0 +1,10 @@
+import os
+
+# Run this script in the directory containing .js and .py repos
+# Ensure that the jscodefiles exist for dumping unique .js files
+# Likewise for pythoncodefiles change the dest dir name in line 10
+for root, dirnames, filenames in os.walk('.'):
+    for filename in filenames:
+        if filename.endswith(('.js', '.py')):
+            filepath = os.path.join(root, filename)
+            os.rename(filepath, './jscodefiles/'+filename)
