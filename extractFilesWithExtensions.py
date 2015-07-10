d74614ec3520469874cbae16e1f8ab14c0588632
Bharat Shetty Barkur <bharat.shetty@gmail.com>
2015-07-11 00:41:08 +0530

    Ensure the corpus directory exists

--- extractFilesWithExtensions.py
+++ extractFilesWithExtensions.py
@@ -1,10 +1,13 @@
 import os
 
+if not os.path.exists('./jscodefiles'):
+    os.makedirs('./jscodefiles')
+
 # Run this script in the directory containing .js and .py repos
 # Ensure that the jscodefiles exist for dumping unique .js files
 # Likewise for pythoncodefiles change the dest dir name in line 10
 for root, dirnames, filenames in os.walk('.'):
     for filename in filenames:
-        if filename.endswith(('.js', '.py')):
+        if filename.endswith(('.js')):
             filepath = os.path.join(root, filename)
             os.rename(filepath, './jscodefiles/'+filename)
