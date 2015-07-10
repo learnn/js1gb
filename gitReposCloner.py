dd07b2a9fbf3bb822c3ef762a7d89efc2c8d7b1b
Bharat Shetty Barkur <bharat.shetty@gmail.com>
2015-07-11 00:25:37 +0530

    Added sequential thread mode for cloning

--- gitReposCloner.py
+++ gitReposCloner.py
@@ -1,7 +1,17 @@
 import os
+import sys
 
-# Text file containing all repos urls is to be fed
-f = open('2.txt', 'r')
+filename = sys.argv[1]
+mode = sys.argv[2]
+print filename
+print mode
+# Text file containing all repos urls is to be fed: input as 1st arg
+# There are two modes it can run:
+#   - mutli thread parallel clone: input 2nd arg as 1
+#   - single thread sequential clone: input 2nd arg 2 
+#
+# Running: python gitReposCloner.py <input_url_list_file> <mode_number>
+f = open(filename, 'r')
 
 lines = f.readlines()
 
@@ -26,8 +36,12 @@ def worker(num):
     return
 
 # spawn worker thread for each repo clone
-threads = []
-for i in range(totalRepoLen):
-    t = threading.Thread(target=worker, args=(i,))
-    threads.append(t)
-    t.start()
+if mode == '1':
+    threads = []
+    for i in range(totalRepoLen):
+        t = threading.Thread(target=worker, args=(i,))
+        threads.append(t)
+        t.start()
+else:
+    for i in range(totalRepoLen):
+        git("clone", repoList[i])
